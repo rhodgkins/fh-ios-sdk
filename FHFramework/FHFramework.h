a782fa071f7534c873ce6c643996b84bd2d1dd3c
Rich Hodgkins <richard.hodgkins@mubaloo.com>
2015-08-14 12:36:50 +0100

    Added framework to support carthage

---
+++ FHFramework/FHFramework.h
@@ -0,0 +1,17 @@
+//
+//  FHFramework.h
+//  FHFramework
+//
+//  Created by Richard Hodgkins on 14/08/2015.
+//  Copyright (c) 2015 FeedHenry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FHFramework.
+FOUNDATION_EXPORT double FHFrameworkVersionNumber;
+
+//! Project version string for FHFramework.
+FOUNDATION_EXPORT const unsigned char FHFrameworkVersionString[];
+
+
